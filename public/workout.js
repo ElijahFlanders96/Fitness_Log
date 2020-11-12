e70bc4844ca3bb9b480308cd0a2ca6c5d30e66b3
Elijah Flanders <elijahflanders15@gmail.com>
2020-11-11 19:33:06 -0600

    edited some stuff on the front-end to correct total workout duration from undefined

--- public/workout.js
+++ public/workout.js
@@ -8,7 +8,7 @@ async function initWorkout() {
   
       const workoutSummary = {
         date: formatDate(lastWorkout.day),
-        totalDuration: lastWorkout.totalDuration,
+        totalDuration: lastWorkout.exercises[0].duration,
         numExercises: lastWorkout.exercises.length,
         ...tallyExercises(lastWorkout.exercises)
       };
@@ -25,8 +25,10 @@ async function initWorkout() {
         acc.totalWeight = (acc.totalWeight || 0) + curr.weight;
         acc.totalSets = (acc.totalSets || 0) + curr.sets;
         acc.totalReps = (acc.totalReps || 0) + curr.reps;
+        acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
       } else if (curr.type === "cardio") {
         acc.totalDistance = (acc.totalDistance || 0) + curr.distance;
+        acc.totalDuration = (acc.totalDuration || 0) + curr.duration;
       }
       return acc;
     }, {});
